e03ed47a5ac45925aca8f3f66f024fa94607dfaa
caffeine23 <hellcrusher23@protonmail.com>
2024-07-17 15:50:50 +0530

    title changes

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -2,7 +2,7 @@ export default function Heading() {
   return (
     <>
       <h1 className="flex justify-center text-3xl font-bold mt-8 text-teal-700">
-        AIO Downloader
+        DownloadIO
       </h1>
     </>
   );
